08cb0e0e2d514ba8cc19b13f0405da37f16474a5
J Scarlatos <jscarlatos42@gmail.com>
2023-05-10 08:53:24 -0400

    finished the js for html and css collapsibles

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,50 @@
-let htmlButton = document.getElementsByClassName('html-button');
+let examples = document.querySelectorAll('.example');
 
-let x = 3;
+function setHtmlVis() {
+    examples.forEach(example => {
+        let hidHtml = example.querySelector('.hidden-html');
+        hidHtml.setAttribute('aria-hidden', 'true');
+    }, 0);
+}
+function showHtml() {
+    examples.forEach(example => {
+        let htmlButton = example.querySelector('.html-button');
+        let hidHtml = example.querySelector('.hidden-html');
+        htmlButton.addEventListener('click', () => {
+            hidHtml.setAttribute('aria-hidden',
+            hidHtml.getAttribute('aria-hidden') === 'true' ? 
+            'false' : 'true');
+            let hidCss = example.querySelector('.hidden-css');
+            hidCss.setAttribute('aria-hidden', 'true');
+        })
+    }, 0);
+}
 
-let y = "string";
+function setCssVis() {
+    examples.forEach(example => {
+        let hidCss = example.querySelector('.hidden-css');
+        hidCss.setAttribute('aria-hidden', 'true');
+    }, 0);
+}
+function showCss() {
+    examples.forEach(example => {
+        let cssButton = example.querySelector('.css-button');
+        let hidCss = example.querySelector('.hidden-css');
+        cssButton.addEventListener('click', () => {
+            hidCss.setAttribute('aria-hidden',
+            hidCss.getAttribute('aria-hidden') === 'true' ? 
+            'false' : 'true');
+            let hidHtml = example.querySelector('.hidden-html');
+            hidHtml.setAttribute('aria-hidden', 'true');
+        })
+    }, 0);
+}
+
+function allFuncs() {
+    setHtmlVis();
+    showHtml();
+    setCssVis();
+    showCss();
+}
+
+window.addEventListener("load", allFuncs);
